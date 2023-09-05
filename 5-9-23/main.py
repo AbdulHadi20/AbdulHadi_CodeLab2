e3b65a5e5e5324e77b0b436f44134e5fc7d4e0fb
Abdul Hadi <abdulhadi2003m@gmail.com>
2023-09-05 21:14:53 +0400

    commit

---
+++ 5-9-23/main.py
@@ -0,0 +1,26 @@
+list1 = [1, 2, 3, 4, 5]
+
+# printing the whole list
+print(list1)
+
+# printing the desired number in the list
+print(list1[2])
+
+# adding an element to a list
+list1.append(6)
+print(list1)
+
+# using arithmetic operations
+print(list1 * 3)
+
+list2 = [7, 8, 9, 10]
+print(list2)
+
+# tuples
+tuple1 = (1, 2, 3, 4, 5)
+
+# printing all the tuples
+print(tuple1)
+
+# printing the desired element in the tuple
+print(tuple1[2])
