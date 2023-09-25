e6af0e66c4d3d811937289acd398f3abb6d37299
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-09-25 23:02:25 +0400

    Update loopsoddnums.py

--- Classwork/19-9-23 (Pyhton Recap Pt. 3)/loopsoddnums.py
+++ Classwork/19-9-23 (Pyhton Recap Pt. 3)/loopsoddnums.py
@@ -1,11 +1,2 @@
 # consider a loop that counts form 1 to 10, but prints only odd numbers
 
-count = 0
-
-while True:
-    if count <= 10 and count % 2 == 1:
-        count += 1
-        print(count)
-
-    else:
-        break
