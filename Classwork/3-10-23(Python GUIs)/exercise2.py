e2010f27f74238df5cd54cb1beb2277b15e9a933
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-10-05 23:00:12 +0400

    Create exercise2.py

---
+++ Classwork/3-10-23(Python GUIs)/exercise2.py
@@ -0,0 +1,29 @@
+# calling the library
+from tkinter import *
+
+
+#creating a function for clicking the button
+#once the button is clicked, the button changes into a label
+def onclick():
+    print("Changed Text")
+
+    #creating a a label
+    theLabel = Label(window, text = "Changed Text")
+    theLabel.pack(side = BOTTOM)
+
+
+#initializing the root window
+window = Tk()
+
+#setting the window title, size dimensions etc.
+window.title("Label Widget")
+window.geometry("800x600")
+
+#creating a button
+theBtn = Button(window, text = "Changed Text", command = onclick)
+theBtn.place(x = 353, y = 550)
+
+#getting the window ready
+window.mainloop()
+
+
