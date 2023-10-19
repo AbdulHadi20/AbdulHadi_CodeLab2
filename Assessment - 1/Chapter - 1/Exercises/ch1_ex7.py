39ff5c366e0a7717c4e6242ea77d7fc5f8774458
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-10-20 00:14:08 +0400

    Update ch1_ex7.py

--- Assessment - 1/Chapter - 1/Exercises/ch1_ex7.py
+++ Assessment - 1/Chapter - 1/Exercises/ch1_ex7.py
@@ -10,30 +10,12 @@ Writing a program that prints the numbers from 1 to 100, but only prints the eve
 # initializing a variable num with 0, so the loop can 
 num = 0
 
-"""
-# creating a while loop to print the numbers continously
-while True:
+# creating a for loop with range from 1 to 101
+for num in range(1, 101):
+    # using conditional statement to check if number is even or odd
+    if num % 2 != 0: # if num is odd, then this condition will execute
+        continue # since the num gets declared as odd, the continue statement helps skipping those numbers that are odd
 
-    # a conditional statement to check if numbers printed are less than or equal to 100
-    if num <= 100:
-        # inrements a number by 1 if the above condition is true
-        num += 1 
-        
-        # a nested conditon to check if the number is even
-        if num % 2 == 0:
-            print(num)
+    print(num) # prints the even numbers instead of odd
 
-        continue # continue statement is used to let the compiler know that the loop should be continued
-
-     # a condition that breaks the loop if the numbers to be printed are greater than 100   
-    else:
-        break # using break to break the loop
-
-# end of the program 
-"""
-
-for num in range(0, 101):
-    if num % 2 != 0:
-        continue
-
-    print(num)
+# end of the program
