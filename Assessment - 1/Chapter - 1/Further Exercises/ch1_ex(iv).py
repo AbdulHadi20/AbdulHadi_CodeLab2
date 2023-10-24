113283fa0f3e372e1a60e034b0a5efc3c606b645
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-10-24 19:44:37 +0400

    Update ch1_ex(iv).py

--- Assessment - 1/Chapter - 1/Further Exercises/ch1_ex(iv).py
+++ Assessment - 1/Chapter - 1/Further Exercises/ch1_ex(iv).py
@@ -13,28 +13,15 @@ staff = ["Arshiya", "Usman", "Iftikhar", "Usman","Rafia", "Mary", "Anmol","Zaina
 # creating a list 
 staff = ["Arshiya", "Usman", "Iftikhar", "Usman","Rafia", "Mary", "Anmol","Zainab","Iftikhar", "Arshiya","Rafia","Jake"]
 
-# displaying the list
-print(f"\n The given list is {staff}")
+# creating a dictionary, where we set the key as i, and its value as zero
+# added a for loop to go through the complete list
+staff_dict = {i:0 for i in staff}
 
-# now, using the count function to count the amount of times an element has appeared in the list
-# creating a seperate variable for each name
-name1 = staff.count("Arshiya")
-name2 = staff.count("Usman")
-name3 = staff.count("Iftikhar")
-name4 = staff.count("Rafia")
-name5 = staff.count("Mary")
-name6 = staff.count("Anmol")
-name7 = staff.count("Zainab")
-name8 = staff.count("Jake")
+# created another for loop to count the items, and then, incrementing the values if the keys are repeated
+for i in staff:
+    staff_dict[i] += 1
 
-# printing the results
-print(f"\n The name {staff[0]} has appeared {name1} times in the list")
-print(f"\n The name {staff[1]} has appeared {name2} times in the list")
-print(f"\n The name {staff[2]} has appeared {name3} times in the list")
-print(f"\n The name {staff[4]} has appeared {name4} times in the list")
-print(f"\n The name {staff[5]} has appeared {name5} times in the list")
-print(f"\n The name {staff[6]} has appeared {name6} times in the list")
-print(f"\n The name {staff[7]} has appeared {name7} times in the list")
-print(f"\n The name {staff[11]} has appeared {name8} times in the list")
+# printing the output
+print(f"\n The dictionary is displayed below: \n {staff_dict}")
 
-# end of the program (TRY ANOTHER WAY)
+# end of the program 
