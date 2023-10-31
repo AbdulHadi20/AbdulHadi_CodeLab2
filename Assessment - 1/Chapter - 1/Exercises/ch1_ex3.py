82026bebd827ec152a4afbdbadd0812ecaaadb95
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-10-31 14:35:41 +0400

    ...

--- Assessment - 1/Chapter - 1/Exercises/ch1_ex3.py
+++ Assessment - 1/Chapter - 1/Exercises/ch1_ex3.py
@@ -26,18 +26,19 @@ sum3 = side1 + side3
 # if the triangle has the sum of any two sides greater or equal to than the remaining side, this statement will executed
 if side1 <= sum2 and side2 <= sum3 and side3 <= sum1: 
     print(f"\n The length of the three sides {side1, side2, side3} make a triangle.")
+
+    # checking the type of the triangle
+    if side1 == side2 == side3: # will execute if all sides are equal
+        print("\n This triangle is an equilateral triangle, because it has all three sides equal.")
+
+    elif side1 == side2 or side2 == side3 or side3 == side1: # will execute if two sides are equal
+        print("\n This triangle is an isosceles triangle, because it has two sides equal.")
+
+    else: # will execute if none of the sides are equal
+        print("\n This triangle is a scalene triangle, because it has no sides equal.")
+
 # this statement will execute if the statement above is false
 else:
     print(f"\n The three sides {side1, side2, side3} do not make a triangle")
-
-# checking the type of the triangle
-if side1 == side2 == side3: # will execute if all sides are equal
-    print("\n This triangle is an equilateral triangle, because it has all three sides equal.")
-
-elif side1 == side2 or side2 == side3 or side3 == side1: # will execute if two sides are equal
-    print("\n This triangle is an isosceles triangle, because it has two sides equal.")
-
-else: # will execute if none of the sides are equal
-    print("\n This triangle is a scalene triangle, because it has no sides equal.")
 
 # end of the program
--- Assessment - 1/Chapter - 1/Exercises/ch1_ex4.py
+++ Assessment - 1/Chapter - 1/Exercises/ch1_ex4.py
@@ -23,5 +23,5 @@ elif num2 > num1 and num3: # will run if the second number is largest
 elif num3 > num1 and num2: # will run if the third number is the largest
     print(f"\n The number {num3} is the largest number.")
 
-# end of the program
+# end of the program (bhai, ye kya hora hai,)
     
