7bb3aad0ca155848e4cb60a7c83e1ccce888f9c0
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-11-05 21:54:45 +0400

    Update ch2_ex1.py

--- Assessment - 1/Chapter - 2/Exercises/ch2_ex1.py
+++ Assessment - 1/Chapter - 2/Exercises/ch2_ex1.py
@@ -13,3 +13,21 @@ Writing a program to develop a GUI to do the following using the tkinder module
 # start of the program 
 
 # calling the tkinter module
+from tkinter import *
+
+# now, we initialize the root window, also we will save the initialization in a variable
+root_window = Tk()
+
+# setting the default window size, and disabling the resizing for the window, because I can
+root_window.geometry("600x600") #sizing
+root_window.resizable(0, 0) #hehe, now it cannot be resized...
+
+# creating a label
+welcome = Label(root_window, text="MY FIRST GUI WINDOW", font="Helvetica", style= "bold", size = 40)
+welcome.pack(side=CENTER)
+
+# adding a background color
+root_window.configure(background="red")
+
+#running the root window
+root_window.mainloop()
