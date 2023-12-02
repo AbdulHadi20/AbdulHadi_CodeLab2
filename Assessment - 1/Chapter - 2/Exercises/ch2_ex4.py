368d91bc29cd379d47ba2154a1a0210913802634
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-12-02 20:37:49 +0400

    ee

--- Assessment - 1/Chapter - 2/Exercises/ch2_ex4.py
+++ Assessment - 1/Chapter - 2/Exercises/ch2_ex4.py
@@ -32,6 +32,9 @@ main_head = Label(main_frame, font = ('Roboto', 25, 'bold'), text = "Student Man
 # creating a sub-heading inside the frame
 subhead = Label(main_frame, font = ('Roboto', 20), text = "New Student Registration", fg = '#ffffff', bg = '#1a2b4c')
 
+# adding the image with the university logo at the top
+bathspalogobanner = PhotoImage(app, file = "bsu_logo_image.png")
+
 # creating the student name text label and the text box for the student name 
 stdname = Label(main_frame, font = ('Roboto', 14), text = "Student Name", fg = '#ffffff', bg = '#1a2b4c')
 stdtext = Text(main_frame, width = 40, height = 2)
@@ -40,16 +43,78 @@ stdtext = Text(main_frame, width = 40, height = 2)
 mobilenum = Label(main_frame, font = ('Roboto', 14), text = "Mobile Number", fg = '#ffffff', bg = '#1a2b4c')
 mobilenumtext = Text(main_frame, width = 40, height = 2)
 
+# creating the label and text box for email id
+email_id = Label(main_frame, font = ('Roboto', 14), text = "Email ID", fg = '#ffffff', bg = '#1a2b4c')
+email_idbox = Text(main_frame, width = 40, height = 2)
+
+# creating the label and text box for home address
+homeaddress = Label(main_frame, font = ('Roboto', 14), text = "Home Address", fg = '#ffffff', bg = '#1a2b4c')
+homeaddressbox = Text(main_frame, width = 40, height = 2)
+
+# creating the label and text box for the gender
+gender = Label(main_frame, font = ('Roboto', 14), text = "Gender", fg = '#ffffff', bg = '#1a2b4c')
+
+# genderdisplay = StringVar()
+# genderdrop = OptionMenu(main_frame, genderdisplay, "Male", "Female", "Confused")
+genderdrop = Text(main_frame, width = 40, height = 2)
+
+# creating a label and radio buttons for courses enrolled
+coursesenrolled = Label(main_frame, font =('Roboto', 14), text = "Course Enrolled", fg = '#ffffff', bg = '#1a2b4c')
+
+# radio buttons
+course_rd1 = Radiobutton(main_frame, text = "BSc CC", bg = '#1a2b4c', fg = '#ffffff')    # radio button 1 
+course_rd2 = Radiobutton(main_frame, text = "BSc CY", bg = '#1a2b4c', fg = '#ffffff')    # radio button 2
+course_rd3 = Radiobutton(main_frame, text = "BSc PSY", bg = '#1a2b4c', fg = '#ffffff' )   # radio button 3
+course_rd4 = Radiobutton(main_frame, text = "BA & BM", bg = '#1a2b4c', fg = '#ffffff')   # radio button 4
+
+# creating a label and checkboxes for the languages known option
+languages = Label(main_frame, font = ('Roboto', 14), text = "Languages known", fg = '#ffffff', bg = '#1a2b4c')
+
+# checkboxes
+checkbox1 = Checkbutton(main_frame, text = 'English', bg = '#1a2b4c', fg = '#ffffff')
+checkbox2 = Checkbutton(main_frame, text = 'Tagalog', bg = '#1a2b4c', fg = '#ffffff')
+checkbox3 = Checkbutton(main_frame, text = 'Hindi/Urdu', bg = '#1a2b4c', fg = '#ffffff')
+
+# creating the text message and its progress meter
+rates = Label(main_frame, font = ('Roboto', 15, 'bold'), text = "Rate your English communication skills", fg = '#ffffff', bg = '#1a2b4c')
+
+
+
 
 ################################## PACKING ALL THE REQURED ITEMS ########################################################
 
+bathspalogobanner.pack(side = TOP)                                 # packing the header image in the window
 main_frame.pack(pady = 20)                                            # packing the main frame inside the app window
 main_head.pack(padx = 10, pady = 10)                                  # packing the main heading inside the mainframe
 subhead.pack()                                                        # packing the subheading inside the mainframe
-stdname.pack(side = LEFT, padx = 30, pady = 60)                       # packing the student name label text in the mainframe
-stdtext.pack(side = RIGHT, padx = 10, pady = 60)                      # packing the textbox for student name   
+stdname.pack()                       # packing the student name label text in the mainframe
+stdtext.pack()                      # packing the textbox for student name   
 mobilenum.pack()                     # packing the mobile number label in the mainframe
 mobilenumtext.pack()                # packing the text box for the mobile number in the mainframe
+email_id.pack()                    # packing the email id label inside the mainframe
+email_idbox.pack()                  # packing the text box for the email inside the mainframe
+homeaddress.pack()                # packing the homeaddress label inside the mainframe
+homeaddressbox.pack()             # packing the homeaddress textbox inside the mainframe
+gender.pack()                    # packing the gender label inside the mainframe
+genderdrop.pack()                 # packing the gender dropdowm menu to select from
+coursesenrolled.pack()           # packing the courses label in the mainframe
 
+# packing all the radio buttons
+course_rd1.pack()             
+course_rd2.pack()             
+course_rd3.pack()             
+course_rd4.pack()    
+
+languages.pack()                    # packing the languages label inside the mainframe
+
+# packing all the checkboxes
+checkbox1.pack()
+checkbox2.pack()
+checkbox3.pack()
+
+rates.pack()                       # packing the communication skills rating message
+#
+
+  
 # using the mainloop function to run the window, loops the program unlimited number of times, until inturepted by anything
 app.mainloop()
