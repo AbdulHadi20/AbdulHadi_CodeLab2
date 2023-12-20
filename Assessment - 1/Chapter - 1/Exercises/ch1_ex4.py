811cb5128ab0a854e8800adea9dfe4c59e29befd
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-12-21 00:31:22 +0400

    Update ch1_ex4.py

--- Assessment - 1/Chapter - 1/Exercises/ch1_ex4.py
+++ Assessment - 1/Chapter - 1/Exercises/ch1_ex4.py
@@ -23,4 +23,7 @@ elif num2 > num1 and num2 > num3: # will run if the second number is largest
 elif num3 > num1 and num3 > num2: # will run if the third number is the largest
     print(f"\n The number {num3} is the largest number.")
 
+else:  # runs if all numbers are equal
+    print("\n All the numbers are same and equal")
+
 # end of the program    
